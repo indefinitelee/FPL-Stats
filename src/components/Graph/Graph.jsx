eeb5cc19221ded445d0c392545a1596b2e28e155
lee <leerwilson@gmail.com>
2017-08-29 14:47:28 -0400

    graph margin

--- src/components/Graph/Graph.jsx
+++ src/components/Graph/Graph.jsx
@@ -10,6 +10,7 @@ const Graph = props => {
         data={props.data}
         height={450}
         width={750}
+        margin={left:10}
         fill={'#234981'}
         title={props.graphTitle}
         yAxisLabel={props.yAxisLabel}
